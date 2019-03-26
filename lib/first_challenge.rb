92c4bdb42c630fbe2ec723049fb67ced80279fbe
Leah Boyd <leahboyd08@gmail.com>
2019-03-26 14:30:39 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,13 @@ def first_challenge
   }
 
   #your code here
-
-
+  contacts.each do |character, info|
+		if character == "Freddy Mercury"
+		  info[:favorite_icecream_flavors].delete_if do |strawberry|
+				strawberry == "strawberry"
+			end 
+		end
+	end
   #remember to return your newly altered contacts hash!
   contacts
 end
