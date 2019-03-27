0134e5b6ada7d92f8a7fead68fb4236fbfa2ed8a
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-27 21:00:54 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,16 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
 
-contacts
+#contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+
+contacts.each {|name, info|
+  info.each {|label, data|
+    if label == :favorite_icecream_flavors
+      data.shift
+    end 
+}
+}
 
   #remember to return your newly altered contacts hash!
   contacts
